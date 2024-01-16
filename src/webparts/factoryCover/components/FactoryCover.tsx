c68b775b4e8fa69bedd671361e800465c808e38c
RubenRedondo <ruben.redondo@wayssen.com.mx>
2024-01-15 23:11:28 -0600

    init

---
+++ src/webparts/factoryCover/components/FactoryCover.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import styles from './FactoryCover.module.scss';
+import type { IFactoryCoverProps } from './IFactoryCoverProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class FactoryCover extends React.Component<IFactoryCoverProps, {}> {
+  public render(): React.ReactElement<IFactoryCoverProps> {
+    const {
+      description,
+      isDarkTheme,
+      environmentMessage,
+      hasTeamsContext,
+      userDisplayName
+    } = this.props;
+
+    return (
+      <section className={`${styles.factoryCover} ${hasTeamsContext ? styles.teams : ''}`}>
+        <div className={styles.welcome}>
+          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
+          <h2>Well done, {escape(userDisplayName)}!</h2>
+          <div>{environmentMessage}</div>
+          <div>Web part property value: <strong>{escape(description)}</strong></div>
+        </div>
+        <div>
+          <h3>Welcome to SharePoint Framework!</h3>
+          <p>
+            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+          </p>
+          <h4>Learn more about SPFx development:</h4>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/webparts/factoryCover/components/IFactoryCoverProps.ts
@@ -0,0 +1,7 @@
+export interface IFactoryCoverProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
